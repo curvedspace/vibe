0e21a1b1ba2e03356ddc3d582cd1739335861929
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2011-10-16 13:53:33 +0200

    Fix build.

--- samples/navbar/main.cpp
+++ samples/navbar/main.cpp
@@ -25,19 +25,19 @@ int main(int argc, char *argv[])
 
     QubeGui::NavBar *navbar = new QubeGui::NavBar();
     QubeGui::NavBarGroup *group = navbar->addGroup("APPLICATIONS");
-    group->addItem(QPixmap(":/icons/z_allApps.png"), "All Applications");
-    group->addItem(QPixmap(":/icons/z_updates.png"), "Updates");
-    group->addItem(QPixmap(":/icons/z_upToDate.png"), "Up To Date");
-    group->addItem(QPixmap(":/icons/z_unknown.png"), "Unknown");
+    group->addItem(QPixmap(":/icons/allApps.png"), "All Applications");
+    group->addItem(QPixmap(":/icons/updates.png"), "Updates");
+    group->addItem(QPixmap(":/icons/upToDate.png"), "Up To Date");
+    group->addItem(QPixmap(":/icons/unknown.png"), "Unknown");
     group = navbar->addGroup("BY KIND");
     group->setStatic(true);
-    group->addItem(QPixmap(":/icons/z_apps.png"), "Applications");
-    group->addItem(QPixmap(":/icons/z_plugins.png"), "Plugins");
-    group->addItem(QPixmap(":/icons/z_widgets.png"), "Widgets");
-    group->addItem(QPixmap(":/icons/z_prefApp.png"), "Preference Panes");
-    group->addItem(QPixmap(":/icons/z_systemUpdates.png"), "System Updates");
+    group->addItem(QPixmap(":/icons/apps.png"), "Applications");
+    group->addItem(QPixmap(":/icons/plugins.png"), "Plugins");
+    group->addItem(QPixmap(":/icons/widgets.png"), "Widgets");
+    group->addItem(QPixmap(":/icons/prefApp.png"), "Preference Panes");
+    group->addItem(QPixmap(":/icons/systemUpdates.png"), "System Updates");
     group = navbar->addGroup("VENDORS");
-    group->addItem(QPixmap(":/icons/z_apple.png"), "Apple");
+    group->addItem(QPixmap(":/icons/apple.png"), "Apple");
     navbar->show();
 
     return app.exec();
