1d602db4f19dd263a4eaaf57ceddea765347a813
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2011-12-17 07:33:26 +0100

    Title widget taken from kdelibs and modified to use QIcon instead of KIcon.

---
+++ src/kits/gui/widgets/vtitlewidget.cpp
@@ -0,0 +1,306 @@
+/****************************************************************************
+ * This file is part of Vibe.
+ *
+ * Copyright (c) 2011 Pier Luigi Fiorini
+ * Copyright (C) 2007-2009 Urs Wolfer
+ * Copyright (C) 2007 Michaël Larouche
+ *
+ * Author(s):
+ *	Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *	Michaël Larouche <larouche@kde.org>
+ *	Urs Wolfer <uwolfer@kde.org>
+ *
+ * Vibe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vibe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Vibe.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
+#include <QTimer>
+#include <QMouseEvent>
+#include <QFrame>
+#include <QIcon>
+#include <QLabel>
+#include <QLayout>
+#include <QTextDocument>
+
+#include "vtitlewidget.h"
+
+/*
+ * VTitleWidget::Private
+ */
+
+class VTitleWidget::Private
+{
+public:
+    Private(VTitleWidget *parent) :
+        q(parent),
+        autoHideTimeout(0),
+        messageType(InfoMessage) {
+    }
+
+    QString textStyleSheet() const {
+        return QString("QLabel { font-weight: bold; color: %1}").arg(q->palette().color(QPalette::WindowText).name());
+    }
+
+    QString commentStyleSheet() const {
+        QString styleSheet;
+        switch (messageType) {
+                //FIXME: we need the usability color styles to implement different
+                //       yet palette appropriate colours for the different use cases!
+                //       also .. should we include an icon here,
+                //       perhaps using the imageLabel?
+            case InfoMessage:
+            case WarningMessage:
+            case ErrorMessage:
+                styleSheet = QString("QLabel { color: palette(%1); background: palette(%2); }").arg(q->palette().color(QPalette::HighlightedText).name()).arg(q->palette().color(QPalette::Highlight).name());
+                break;
+            case PlainMessage:
+            default:
+                break;
+        }
+        return styleSheet;
+    }
+
+    VTitleWidget *q;
+    QGridLayout *headerLayout;
+    QLabel *imageLabel;
+    QLabel *textLabel;
+    QLabel *commentLabel;
+    int autoHideTimeout;
+    MessageType messageType;
+
+    /**
+     * @brief Get the icon name from the icon type
+     * @param type icon type from the enum
+     * @return named icon as QString
+     */
+    QString iconTypeToIconName(VTitleWidget::MessageType type);
+
+    void _q_timeoutFinished() {
+        q->setVisible(false);
+    }
+};
+
+QString VTitleWidget::Private::iconTypeToIconName(VTitleWidget::MessageType type)
+{
+    switch (type) {
+        case VTitleWidget::InfoMessage:
+            return QLatin1String("dialog-information");
+            break;
+        case VTitleWidget::ErrorMessage:
+            return QLatin1String("dialog-error");
+            break;
+        case VTitleWidget::WarningMessage:
+            return QLatin1String("dialog-warning");
+            break;
+        case VTitleWidget::PlainMessage:
+            break;
+    }
+
+    return QString();
+}
+
+/*
+ * VTitleWidget
+ */
+
+VTitleWidget::VTitleWidget(QWidget *parent) :
+    QWidget(parent),
+    d(new Private(this))
+{
+    QFrame *titleFrame = new QFrame(this);
+    titleFrame->setAutoFillBackground(true);
+    titleFrame->setFrameShape(QFrame::StyledPanel);
+    titleFrame->setFrameShadow(QFrame::Plain);
+    titleFrame->setBackgroundRole(QPalette::Base);
+
+    // default image / text part start
+    d->headerLayout = new QGridLayout(titleFrame);
+    d->headerLayout->setColumnStretch(0, 1);
+    d->headerLayout->setMargin(6);
+
+    d->textLabel = new QLabel(titleFrame);
+    d->textLabel->setVisible(false);
+    d->textLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::LinksAccessibleByMouse);
+
+    d->imageLabel = new QLabel(titleFrame);
+    d->imageLabel->setVisible(false);
+
+    d->headerLayout->addWidget(d->textLabel, 0, 0);
+    d->headerLayout->addWidget(d->imageLabel, 0, 1, 1, 2);
+
+    d->commentLabel = new QLabel(titleFrame);
+    d->commentLabel->setVisible(false);
+    d->commentLabel->setOpenExternalLinks(true);
+    d->commentLabel->setWordWrap(true);
+    d->commentLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::LinksAccessibleByMouse);
+    d->headerLayout->addWidget(d->commentLabel, 1, 0);
+
+    // default image / text part end
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(titleFrame);
+    mainLayout->setMargin(0);
+    setLayout(mainLayout);
+}
+
+VTitleWidget::~VTitleWidget()
+{
+    delete d;
+}
+
+bool VTitleWidget::eventFilter(QObject *object, QEvent *event)
+{
+    // Hide message label on click
+    if (d->autoHideTimeout > 0 &&
+            event->type() == QEvent::MouseButtonPress) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        if (mouseEvent && mouseEvent->button() == Qt::LeftButton) {
+            setVisible(false);
+            return true;
+        }
+    }
+
+    return QWidget::eventFilter(object, event);
+}
+
+void VTitleWidget::setWidget(QWidget *widget)
+{
+    d->headerLayout->addWidget(widget, 2, 0, 1, 2);
+}
+
+QString VTitleWidget::text() const
+{
+    return d->textLabel->text();
+}
+
+QString VTitleWidget::comment() const
+{
+    return d->commentLabel->text();
+}
+
+const QPixmap *VTitleWidget::pixmap() const
+{
+    return d->imageLabel->pixmap();
+}
+
+void VTitleWidget::setBuddy(QWidget *buddy)
+{
+    d->textLabel->setBuddy(buddy);
+}
+
+void VTitleWidget::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    if (e->type() == QEvent::PaletteChange) {
+        d->textLabel->setStyleSheet(d->textStyleSheet());
+        d->commentLabel->setStyleSheet(d->commentStyleSheet());
+    }
+}
+
+void VTitleWidget::setText(const QString &text, Qt::Alignment alignment)
+{
+    d->textLabel->setVisible(!text.isNull());
+
+    if (!Qt::mightBeRichText(text)) {
+        d->textLabel->setStyleSheet(d->textStyleSheet());
+    }
+
+    d->textLabel->setText(text);
+    d->textLabel->setAlignment(alignment);
+    show();
+}
+
+void VTitleWidget::setText(const QString &text, MessageType type)
+{
+    setPixmap(type);
+    setText(text);
+}
+
+void VTitleWidget::setComment(const QString &comment, MessageType type)
+{
+    d->commentLabel->setVisible(!comment.isNull());
+
+    //TODO: should we override the current icon with the corresponding MessageType icon?
+    d->messageType = type;
+    d->commentLabel->setStyleSheet(d->commentStyleSheet());
+    d->commentLabel->setText(comment);
+    show();
+}
+
+void VTitleWidget::setPixmap(const QPixmap &pixmap, ImageAlignment alignment)
+{
+    d->imageLabel->setVisible(!pixmap.isNull());
+
+    d->headerLayout->removeWidget(d->textLabel);
+    d->headerLayout->removeWidget(d->commentLabel);
+    d->headerLayout->removeWidget(d->imageLabel);
+
+    if (alignment == ImageLeft) {
+        // swap the text and image labels around
+        d->headerLayout->addWidget(d->imageLabel, 0, 0, 2, 1);
+        d->headerLayout->addWidget(d->textLabel, 0, 1);
+        d->headerLayout->addWidget(d->commentLabel, 1, 1);
+        d->headerLayout->setColumnStretch(0, 0);
+        d->headerLayout->setColumnStretch(1, 1);
+    } else {
+        d->headerLayout->addWidget(d->textLabel, 0, 0);
+        d->headerLayout->addWidget(d->commentLabel, 1, 0);
+        d->headerLayout->addWidget(d->imageLabel, 0, 1, 2, 1);
+        d->headerLayout->setColumnStretch(1, 0);
+        d->headerLayout->setColumnStretch(0, 1);
+    }
+
+    d->imageLabel->setPixmap(pixmap);
+}
+
+
+void VTitleWidget::setPixmap(const QString &icon, ImageAlignment alignment)
+{
+    setPixmap(QIcon::fromTheme(icon), alignment);
+}
+
+void VTitleWidget::setPixmap(const QIcon &icon, ImageAlignment alignment)
+{
+    // TODO: We definitely need to take this from a settings singleton
+    setPixmap(icon.pixmap(22, 22), alignment);
+}
+
+void VTitleWidget::setPixmap(MessageType type, ImageAlignment alignment)
+{
+    setPixmap(QIcon::fromTheme(d->iconTypeToIconName(type)), alignment);
+}
+
+int VTitleWidget::autoHideTimeout() const
+{
+    return d->autoHideTimeout;
+}
+
+void VTitleWidget::setAutoHideTimeout(int msecs)
+{
+    d->autoHideTimeout = msecs;
+
+    if (msecs > 0)
+        installEventFilter(this);
+    else
+        removeEventFilter(this);
+}
+
+void VTitleWidget::showEvent(QShowEvent *event)
+{
+    Q_UNUSED(event)
+
+    if (d->autoHideTimeout > 0)
+        QTimer::singleShot(d->autoHideTimeout, this, SLOT(_q_timeoutFinished()));
+}
+
+#include "vtitlewidget.moc"
---
+++ src/kits/gui/widgets/vtitlewidget.h
@@ -0,0 +1,228 @@
+/****************************************************************************
+ * This file is part of Vibe.
+ *
+ * Copyright (c) 2011 Pier Luigi Fiorini
+ * Copyright (C) 2007-2009 Urs Wolfer
+ * Copyright (C) 2007 Michaël Larouche
+ *
+ * Author(s):
+ *	Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *	Michaël Larouche <larouche@kde.org>
+ *	Urs Wolfer <uwolfer@kde.org>
+ *
+ * Vibe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vibe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Vibe.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
+#ifndef VTITLEWIDGET_H
+#define VTITLEWIDGET_H
+
+#include <QWidget>
+
+#include <VibeCore/VGlobal>
+
+/** \addtogroup gui Gui Kit
+ *  @{
+ */
+
+/**
+ * \class VTitleWidget vtitlewidget.h <VibeGui/VTitleWidget>
+ *
+ * @short Standard title widget with a white background and round border.
+ *
+ * This class provides a widget often used for dialog titles.
+ * \image html vtitlewidget.png "VTitleWidget with title and icon"
+ *
+ * @section Usage
+ * VTitleWidget is very simple to use. You can either use its default text
+ * (and pixmap) properties or display your own widgets in the title widget.
+ *
+ * A title text with a left aligned pixmap:
+ * @code
+ * VTitleWidget *titleWidget = new VTitleWidget(this);
+ * titleWidget->setText(tr("Title"));
+ * titleWidget->setPixmap(QIcon::fromTheme("screen").pixmap(22, 22), VTitleWidget::ImageLeft);
+ * @endcode
+ *
+ * Use it with an own widget:
+ * @code
+ * VTitleWidget *checkboxTitleWidget = new VTitleWidget(this);
+ *
+ * QWidget *checkBoxTitleMainWidget = new QWidget(this);
+ * QVBoxLayout *titleLayout = new QVBoxLayout(checkBoxTitleMainWidget);
+ * titleLayout->setMargin(6);
+ *
+ * QCheckBox *checkBox = new QCheckBox("Text Checkbox", checkBoxTitleMainWidget);
+ * titleLayout->addWidget(checkBox);
+ *
+ * checkboxTitleWidget->setWidget(checkBoxTitleMainWidget);
+ * @endcode
+ *
+ * @see VPageView
+ *
+ * @author Urs Wolfer <uwolfer@kde.org>
+ * @author Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ */
+class VIBE_EXPORT VTitleWidget : public QWidget
+{
+    Q_OBJECT
+    Q_ENUMS(ImageAlignment)
+    Q_PROPERTY(QString text READ text WRITE setText)
+    Q_PROPERTY(QString comment READ comment WRITE setComment)
+    Q_PROPERTY(QPixmap pixmap READ pixmap WRITE setPixmap)
+    Q_PROPERTY(int autoHideTimeout READ autoHideTimeout WRITE setAutoHideTimeout)
+public:
+    /**
+     * Possible title pixmap alignments.
+     *
+     * @li ImageLeft: Display the pixmap left
+     * @li ImageRight: Display the pixmap right (default)
+     */
+    enum ImageAlignment {
+        ImageLeft, /**< Display the pixmap on the left */
+        ImageRight /**< Display the pixmap on the right */
+    };
+
+    /**
+     * Comment message types
+     */
+    enum MessageType {
+        PlainMessage, /**< Normal comment */
+        InfoMessage, /**< Information the user should be alerted to */
+        WarningMessage, /**< A warning the user should be alerted to */
+        ErrorMessage /**< An error message */
+    };
+
+    /**
+     * Constructs a title widget with the given @param parent.
+     */
+    explicit VTitleWidget(QWidget *parent = 0);
+
+    /**
+     * Destroys the title widget.
+     */
+    virtual ~VTitleWidget();
+
+    /**
+     * @param widget Widget displayed on the title widget.
+     */
+    void setWidget(QWidget *widget);
+
+    /**
+     * @return the text displayed in the title
+     * @see setText()
+     */
+    QString text() const;
+
+    /**
+     * @return the text displayed in the comment below the title, if any
+     * @see setComment()
+     */
+    QString comment() const;
+
+    /**
+     * @return the pixmap displayed in the title
+     * @see setPixmap()
+     */
+    const QPixmap *pixmap() const;
+
+    /**
+     * Sets this label's buddy to buddy.
+     * When the user presses the shortcut key indicated by the label in this
+     * title widget, the keyboard focus is transferred to the label's buddy
+     * widget.
+     * @param buddy the widget to activate when the shortcut key is activated
+     */
+    void setBuddy(QWidget *buddy);
+
+    /**
+     * Get the current timeout value in milliseconds
+     * @return timeout value in msecs
+     */
+    int autoHideTimeout() const;
+
+public slots:
+    /**
+     * @param text Text displayed on the label. It can either be plain text or rich text. If it
+     * is plain text, the text is displayed as a bold title text.
+     * @param alignment Alignment of the text. Default is left and vertical centered.
+     * @see text()
+     */
+    void setText(const QString &text, Qt::Alignment alignment = Qt::AlignLeft | Qt::AlignVCenter);
+    /**
+     * @param text Text displayed on the label. It can either be plain text or rich text. If it
+     * is plain text, the text is displayed as a bold title text.
+     * @param type The sort of message it is; will also set the icon accordingly @see MessageType
+     * @see text()
+     */
+    void setText(const QString &text, MessageType type);
+
+    /**
+     * @param comment Text displayed beneath the main title as a comment.
+     *                It can either be plain text or rich text.
+     * @param type The sort of message it is. @see MessageType
+     * @see comment()
+     */
+    void setComment(const QString &comment, MessageType type = PlainMessage);
+
+    /**
+     * @param pixmap Pixmap displayed in the header. The pixmap is by default right, but
+     * @param alignment can be used to display it also left.
+     * @see pixmap()
+     */
+    void setPixmap(const QPixmap &pixmap, ImageAlignment alignment = ImageRight);
+
+    /**
+     * @param icon name of the icon to display in the header. The pixmap is by default right, but
+     * @param alignment can be used to display it also left.
+     * @see pixmap()
+     */
+    void setPixmap(const QString &icon, ImageAlignment alignment = ImageRight);
+
+    /**
+     * @param pixmap the icon to display in the header. The pixmap is by default right, but
+     * @param alignment can be used to display it also left.
+     * @see pixmap()
+     */
+    void setPixmap(const QIcon &icon, ImageAlignment alignment = ImageRight);
+
+    /**
+     * @param pixmap the icon to display in the header. The pixmap is by default right, but
+     * @param alignment can be used to display it also left.
+     * @see pixmap()
+     */
+    void setPixmap(MessageType type, ImageAlignment alignment = ImageRight);
+
+    /**
+     * Set the autohide timeout of the label
+     * Set value to 0 to disable autohide, which is the default.
+     * @param msecs timeout value in milliseconds
+     */
+    void setAutoHideTimeout(int msecs);
+
+protected:
+    void changeEvent(QEvent *e);
+    void showEvent(QShowEvent *event);
+    bool eventFilter(QObject *object, QEvent *event);
+
+private:
+    class Private;
+    Private *const d;
+
+    Q_PRIVATE_SLOT(d, void _q_timeoutFinished())
+    Q_DISABLE_COPY(VTitleWidget)
+};
+
+/** @}*/
+
+#endif // VTITLEWIDGET_H
